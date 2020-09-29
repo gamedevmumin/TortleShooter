c37ca797722aee14ea8e8aea57d58208a05b0753
gamedevmumin <gamedevmumin@wp.pl>
2020-09-29 18:32:23 +0200

    [GAME-2]Weapon system - reloading

--- Assets/Player/Weapons/Scripts/WeaponStats.cs
+++ Assets/Player/Weapons/Scripts/WeaponStats.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class WeaponStats : ScriptableObject
-{
+public class WeaponStats : ScriptableObject {
     [SerializeField]
     Bullet bullet;
     [SerializeField]
@@ -15,10 +14,13 @@ public class WeaponStats : ScriptableObject
     string shotSound;
     [SerializeField]
     float shotsInterval;
+    [SerializeField]
+    float reloadTime;
 
     public Bullet Bullet { get => bullet; private set => bullet = value; }
     public float ShakeAmount { get => shakeAmount; private set => shakeAmount = value; }
     public float Spread { get => spread; private set => spread = value; }
     public string ShotSound { get => shotSound; private set => shotSound = value; }
     public float ShotsInterval { get => shotsInterval; private set => shotsInterval = value; }
+    public float ReloadTime { get => reloadTime; private set => reloadTime = value; }
 }
