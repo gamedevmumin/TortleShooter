2080df2a9c4fe487b6c2695705a4495d6c0e3d05
gamedevmumin <gamedevmumin@wp.pl>
2020-10-01 15:35:41 +0200

    added reloading mechanic

--- Assets/Player/Weapons/Scripts/DefaultMagazineManager.cs
+++ Assets/Player/Weapons/Scripts/DefaultMagazineManager.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DefaultMagazineManager : MonoBehaviour, IWeaponMagazineManager
-{
+public class DefaultMagazineManager : MonoBehaviour, IWeaponMagazineManager {
     private WeaponStats stats;
-	private int bulletsInMagazine;
+    private int bulletsInMagazine;
+    private bool isReloading;
 
-    public void Initialize(WeaponStats stats) {
+    void Update () {
+        if (Input.GetButton ("Reload") && isReloading == false) {
+            StartCoroutine(Reload());
+        }
+    }
+
+    public void Initialize (WeaponStats stats) {
         this.stats = stats;
         bulletsInMagazine = stats.MagazineSize;
     }
 
-    public bool IsMagazineEmpty() {
+    public bool IsMagazineEmpty () {
         return bulletsInMagazine <= 0;
     }
 
-    public void ChangeBulletsAmountByNumber(int amount) {
+    public void ChangeBulletsAmountByNumber (int amount) {
         bulletsInMagazine += amount;
+    }
+
+    IEnumerator Reload () {
+        isReloading = true;
+        yield return new WaitForSeconds(stats.ReloadTime);
+        bulletsInMagazine = stats.MagazineSize;
+        isReloading = false;
     }
 }
