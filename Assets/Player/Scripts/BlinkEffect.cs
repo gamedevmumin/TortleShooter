b5bec8dbf222f2b2d1f8b46174670006c65e9fa2
Łukasz Ochwat <41166458+gamedevmumin@users.noreply.github.com>
2021-03-13 12:48:28 +0100

    [GAME-24] Add lightning to the game (#10)
    * changed old shaders for urp
    
    * adjusted all scenes to work with shaders

--- Assets/Player/Scripts/BlinkEffect.cs
+++ Assets/Player/Scripts/BlinkEffect.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 [CreateAssetMenu]
 public class BlinkEffect : VisualSpriteEffect
 {
+    private static readonly int Blink = Shader.PropertyToID("_Blink");
 
     public override IEnumerator PlayEffect(SpriteRenderer sR)
     {
-        sR.material.SetFloat("_FlashAmount", 1f);
-        yield return new WaitForSeconds(0.03f);
-        sR.material.SetFloat("_FlashAmount", 0f);
-    } 
+        sR.material.SetInt(Blink, 1);
+        yield return new WaitForSeconds(0.08f);
+        sR.material.SetInt(Blink, 0);    
+    }
 }
